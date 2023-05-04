bc6ba5892d6d17d90ea94669be1d95f9f413fbad
tnp2004 <iceandiceisice2004@gmail.com>
2023-05-04 14:20:14 +0700

    Server: Serapate function from method function

--- server/main.go
+++ server/main.go
@@ -12,58 +12,63 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Get("/api/poll", func(c *fiber.Ctx) error {
-		return c.JSON(database.GetPolls())
-	})
-
-	app.Post("/api/poll/new", func(c *fiber.Ctx) error {
-		poll := &database.Poll{}
-
-		if err := c.BodyParser(poll); err != nil {
-			return err
-		}
-		pollsData := database.InsertPoll(*poll)
-
-		return c.JSON(pollsData)
-	})
-
-	app.Delete("/api/poll/delete/:id", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id")
-		fmt.Println(id)
-		if err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "Poll id must be a numbers only",
-			})
-		}
-
-		pollsData, err := database.DeletePoll(id)
-		if err != nil {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": err.Error(),
-			})
-		}
-
-		return c.JSON(pollsData)
-	})
-
-	app.Patch("/api/poll/update/:id/:option", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id")
-		if err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "Poll id must be a numbers only",
-			})
-		}
-		option := c.Params("option")
-
-		pollsData, err := database.UpdatePoint(id, option)
-		if err != nil {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": err.Error(),
-			})
-		}
-
-		return c.JSON(pollsData)
-	})
+	app.Get("/api/poll", GetPolls)
+	app.Post("/api/poll/new", NewPoll)
+	app.Delete("/api/poll/delete/:id", DeletePoll)
+	app.Patch("/api/poll/update/:id/:option", UpdatePoll)
 
 	log.Fatal(app.Listen(":4000"))
+}
+
+func GetPolls(c *fiber.Ctx) error {
+	return c.JSON(database.GetPolls())
+}
+
+func NewPoll(c *fiber.Ctx) error {
+	poll := &database.Poll{}
+
+	if err := c.BodyParser(poll); err != nil {
+		return err
+	}
+	pollsData := database.InsertPoll(*poll)
+
+	return c.JSON(pollsData)
+}
+
+func DeletePoll(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	fmt.Println(id)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Poll id must be a numbers only",
+		})
+	}
+
+	pollsData, err := database.DeletePoll(id)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(pollsData)
+}
+
+func UpdatePoll(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Poll id must be a numbers only",
+		})
+	}
+	option := c.Params("option")
+
+	pollsData, err := database.UpdatePoint(id, option)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(pollsData)
 }
